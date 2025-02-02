649054adbbc6d72b0a135576b127b90af3ad6c29
Gabriel Bunhak <bunhakgabriel@gmail.com>
2025-02-02 17:27:00 -0300

    corrige erro de dependencias

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "./services/firebase";
 import { doc, setDoc } from "firebase/firestore"; // Importando métodos corretos
 import Login from "./components/Login/Login";
-// import Loading from "./components/Loading/Loading";
 import Sidebar from './components/Sidebar';
 import * as C from "./styles/app";
 import Chat from './components/Chat/Index';
@@ -21,7 +20,6 @@ const App = () => {
         }
     }, [user]);
 
-    // if (loading) return <Loading />;
     if (!user) return <Login />;
 
     return (
